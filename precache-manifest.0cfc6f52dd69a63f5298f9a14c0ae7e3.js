6a692cbda73244eb76588a345b7c068fdce59dc3
Gevorg <57664282+Gevorgpro@users.noreply.github.com>
2020-07-17 16:39:26 +0400

    Add files via upload

---
+++ precache-manifest.0cfc6f52dd69a63f5298f9a14c0ae7e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca185d8a44dc0b02226e95a586178d1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0a7aa8409b51b614608",
+    "url": "/static/css/main.a4bd6fbf.chunk.css"
+  },
+  {
+    "revision": "f203ef3d3e44e7ba48a6",
+    "url": "/static/js/2.c727b9b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c727b9b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0a7aa8409b51b614608",
+    "url": "/static/js/main.9b0b5e81.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "02e0f48a44070ea28ab1ea818e175aa7",
+    "url": "/static/media/aa.02e0f48a.jpg"
+  }
+]);
